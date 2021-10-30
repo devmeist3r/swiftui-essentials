8ac2b3c7f342fcbdaa53050e62ede0927e9b6d39
devmeist3r <devmeist3r@gmail.com>
2021-10-30 20:43:16 -0300

    *

---
+++ GridDemo/GridDemo/GridDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridDemoApp.swift
+//  GridDemo
+//
+//  Created by Lucas Inocencio on 30/10/21.
+//
+
+import SwiftUI
+
+@main
+struct GridDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
